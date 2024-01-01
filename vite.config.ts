6e4ca6cf538f269c6679d5e73557f24acdcfcc4f
needForBeans <lucamrickli@gmail.com>
2024-01-01 01:40:05 +0100

    pathprefix fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from "vite";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
-import { fileURLToPath, resolve } from "node:url";
+import { fileURLToPath } from "node:url";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -10,4 +10,5 @@ export default defineConfig({
     },
   },
   plugins: [svelte()],
+  base: "/mme/",
 });
